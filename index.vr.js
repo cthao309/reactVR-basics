07d005a35e62fb651ecfdf215e8ac36da7015405
Charlie Thao <cthao309@gmail.com>
2020-06-26 15:43:24 -0700

    creating sub component with todo list

--- index.vr.js
+++ index.vr.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
-import { AppRegistry, asset, Pano, View } from 'react-vr';
+import { AppRegistry, asset, Pano, View, Text } from 'react-vr';
+
+class Task extends Component {
+  render() {
+    return (
+      <Text style={{textAlign: 'center', fontSize: .2}}>Too: Get groceries.</Text>
+    )
+  }
+}
+
 
 export default class Basics extends Component {
   render() {
     return (
       <View>
         <Pano source={asset('starry-sky.jpg')}></Pano>
+        <view
+          style={{
+            transform: [{translate: [0, 0, -3]}],
+            layoutOrigin: [.5, .5]
+          }}
+        >
+          <Task />
+          <Task />
+          <Task />
+        </view>
       </View>
     )
   }
